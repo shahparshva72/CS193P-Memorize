7ca797863fbd5a2fa4d3b47a7dc2cb223c2bb841
Parshva Shah <shahparshva72@gmail.com>
2021-05-28 15:05:10 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Parshva Shah on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
